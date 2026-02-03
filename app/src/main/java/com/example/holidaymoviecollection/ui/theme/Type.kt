e609c192265b55a858ff9796b01e04b7f2bf7524
mdidomizio <miriam.didomizio@googlemail.com>
2026-02-03 16:31:30 +0100

    adding font

---
+++ app/src/main/java/com/example/holidaymoviecollection/ui/theme/Type.kt
@@ -0,0 +1,53 @@
+package com.example.holidaymoviecollection.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.holidaymoviecollection.R
+
+// Set of Material typography styles to start with
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plus_jakarta_sans_regular, FontWeight.Normal),
+    Font(R.font.plus_jakarta_sans_medium, FontWeight.Medium),
+    Font(R.font.plus_jakarta_sans_bold, FontWeight.Bold),
+    Font(R.font.plus_jakarta_sans_semi_bold, FontWeight.SemiBold)
+)
+
+val AppTypography = Typography(
+    bodyLarge = Typography().bodyLarge.copy(
+        fontFamily = PlusJakartaSans
+    ),
+    titleLarge = Typography().titleLarge.copy(
+        fontFamily = PlusJakartaSans
+    ),
+    labelLarge = Typography().labelLarge.copy(
+        fontFamily = PlusJakartaSans
+    )
+)
+
+/*val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )*/
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
